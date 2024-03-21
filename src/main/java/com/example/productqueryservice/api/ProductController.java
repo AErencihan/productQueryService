0c401fb8fbe83dd8003aaabf00685c5aa2aeb984
AErencihan <eren.cihann@hotmail.com>
2024-03-21 21:51:32 +0300

    added and changed

--- src/main/java/com/example/productqueryservice/api/ProductController.java
+++ src/main/java/com/example/productqueryservice/api/ProductController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/product")
+@RequestMapping("/query/api/v1/product")
 @RequiredArgsConstructor
 @Slf4j
 public class ProductController {
